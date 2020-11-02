e2582ee9641d52a655975174cf6d2adff7f63664
Heba10 <hebe10@yahoo.com>
2020-11-02 09:52:09 +0200

    trainers CRUD 2 delete images using Storage class

--- app/Http/Controllers/Admin/TrainerController.php
+++ app/Http/Controllers/Admin/TrainerController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Trainer;
 use Image;
+use Illuminate\Support\Facades\Storage;
 
 class TrainerController extends Controller
 {
@@ -49,16 +50,31 @@ class TrainerController extends Controller
 
     public function edit($id)
     {
-        $data['cat'] = Trainer::findOrFail($id);
+        $data['trainer'] = Trainer::findOrFail($id);
         return view('admin.trainers.edit')->with($data);
     }
 
     public function update(Request $request){
      
         $data = $request->validate([
-              'name' => 'required|string|max:20'
+            'name' => 'required|string|max:50',
+            'speciality' => 'required|string|max:50',
+            'phone' => 'nullable|string|max:20',
+            'img' => 'nullable|image|mimes:jpg,jpeg,png',
 
         ]);
+
+        $old_name = Trainer::findOrFail($request->id)->img;
+
+        if($request->hasFile('img')){
+            Storage::disk('uploads')->delete('trainers/'. $old_name);
+          $new_name=$data['img']->hashName();
+          Image::make($data['img'])->resize(50,50)->save(public_path('uploads/trainers/' . $new_name)); 
+          $data['img']= $new_name;  
+        }
+        else{
+            $data['img'] = $old_name;
+        }
 
           Trainer::findOrFail($request->id)->update($data);
           return back();
@@ -67,7 +83,8 @@ class TrainerController extends Controller
     }
 
     public function delete($id){
-     
+        $old_name = Trainer::findOrFail($id)->img;
+        Storage::disk('uploads')->delete('trainers/'. $old_name);
         Trainer::findOrFail($id)->delete();
         return back();
 
--- resources/views/admin/trainers/edit.blade.php
+++ resources/views/admin/trainers/edit.blade.php
@@ -4,18 +4,34 @@
 
 <div class="container p-5 m-5">
 <div class="d-flex justify-content-between mb-3">
-<h6>Categories / Edit / {{ $cat->name}}</h6>
-<a class="btn btn-sm btn-primary" href="{{route('admin.cats.index')}}">Back</a>
+<h6>Trainers / Edit / {{ $trainer->name}}</h6>
+<a class="btn btn-sm btn-primary" href="{{route('admin.trainers.index')}}">Back</a>
 </div>
 
 
-<form method="POST" action="{{route('admin.cats.update')}}">
+<form method="POST" action="{{route('admin.trainers.update')}}" enctype="multpart/form-data">
 @csrf
-<input type="hidden" name="id" value="{{$cat->id}}">
+<input type="hidden" name="id" value="{{$trainer->id}}">
 <div class="form-group">
 <label>Name</label>
-<input type="text" name="name" class="form-control" value="{{$cat->name}}">
+<input type="text" name="name" class="form-control" value="{{$trainer->name}}">
 </div>
+<div class="form-group">
+<label>phone</label>
+<input type="text" name="phone" class="form-control" value="{{$trainer->phone}}">
+</div>
+
+<div class="form-group">
+<label>speciality</label>
+<input type="text" name="speciality" class="form-control" value="{{$trainer->speciality}}">
+</div>
+
+<img src="{{ asset('uploads/trainers/'. $trainer->img) }}" height="100px" alt="" class="my-5">
+<div class="form-group">
+ 
+<input type="file" name="img" class="form-control-file">
+</div>
+
 
 <button type="submit" class="btn btn-primary">Submit</button>
 
